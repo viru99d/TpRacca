15b76de50b4ad200f7e309d0e4974945b772f411
Gonzalo Alvarez <gonzalo.alvarez010@outlook.com>
2020-06-11 15:33:01 -0300

    123

---
+++ inicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FotocopiadoraFacultad
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection Con = new SqlConnection("Data Source =.; Initial Catalog = "Control de Pedidos Fotocopiadora UTN"; Integrated Security = True");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
