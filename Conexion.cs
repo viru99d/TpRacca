c0ad362363fb767c535d3bd2cc2c376c496e858b
Gonzalo Alvarez <gonzalo.alvarez010@outlook.com>
2020-06-12 18:31:41 -0300

    123

---
+++ Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace FotocopiadoraFacultad
+{
+    class Conexion
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+
+    }
+}
--- inicio.cs
+++ inicio.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
 using System.Data.SqlClient;
+
 
 namespace FotocopiadoraFacultad
 {
@@ -18,28 +20,7 @@ namespace FotocopiadoraFacultad
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnInicio_Click(object sender, EventArgs e)
         {
             try
             {
@@ -53,5 +34,60 @@ namespace FotocopiadoraFacultad
                 MessageBox.Show("Ocurrio un error, no se puedo conectar a la base de datos " + error.Message);
             }
         }
+
+        private void Nombre_LostFocus(object sender, EventArgs e)
+        {
+            controlBotonNombre();
+        }
+
+        private void Contraseña_LostFocus(object sender, EventArgs e)
+        {
+            controlBotonContraseña();
+        }
+
+
+        private void controlBotonNombre()
+        {
+            if (Nombre.Text.Trim() == "Fotocopiadora 123")
+            {
+                inicioSesión.Enabled = true;
+                errorProvider1.SetError(Nombre, "");
+            }
+            else
+            {
+                errorProvider1.SetError(Nombre, "El usuario es incorrecto");
+                inicioSesión.Enabled = false;
+                Nombre.Focus();
+            }
+        }
+
+        private void controlBotonContraseña()
+        {
+            if (Contraseña.Text.Trim() == "12345678")
+            {
+                inicioSesión.Enabled = true;
+                errorProvider2.SetError(Contraseña, "");
+            }
+            else
+            {
+                errorProvider2.SetError(Contraseña, "La contraseña es incorrecta");
+                inicioSesión.Enabled = false;
+                Contraseña.Focus();
+            }
+        }
+
+        private void inicioSesión_Click(object sender, EventArgs e)
+        {
+            inicioSesión.Enabled = false;
+            using (InicioDos ventanaInicioDos = new InicioDos())
+                ventanaInicioDos.ShowDialog();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
     }
 }
